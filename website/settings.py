ed3ba0e3057a9b260a8a0b078a23d90d5f49797b
thbinhh <94433293+thbinhh@users.noreply.github.com>
2023-06-06 03:46:49 +0700

    test

--- website/settings.py
+++ website/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIRS = [
-    BASE_DIR.joinpath('static'),
+    BASE_DIR.joinpath('ecommerce/static'),
 ]
 
 
